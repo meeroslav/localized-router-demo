0e67eed5b4dd46e8f8e5afc5a7216991405ac462
Miroslav Jonas <missing.manual@gmail.com>
2021-04-19 09:46:44 +0200

    feat: add styles;

--- apps/localized-router-demo/src/app/app.module.ts
+++ apps/localized-router-demo/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ComponentRouterModule } from '@angular-component/router';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    ComponentRouterModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
