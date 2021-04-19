509fa206eb3a7509a0d52d8ee2f9027fa38bc3a7
Miroslav Jonas <missing.manual@gmail.com>
2021-04-19 14:26:03 +0200

    feat: add language switcher

--- apps/localized-router-demo/src/app/app.component.ts
+++ apps/localized-router-demo/src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { TranslateService } from './translate/translate.service';
 
 @Component({
-  selector: 'localized-router-demo-root',
+  selector: 'demo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'localized-router-demo';
+  languages;
+
+  constructor(private readonly translate: TranslateService) {
+    this.languages = this.translate.languages;
+  }
 
   usersLoader = () => import('./users/users.module').then(m => m.UsersModule);
+
+  changeLanguage(lang: string) {
+    this.translate.language = lang;
+  }
 }
